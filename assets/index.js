80a718b9cec38243fde8160fdd5afe089cf823d4
Nixon Mathew Nelson <1951@solvezy.com>
2019-08-22 17:14:27 +0530

    Init Commit

---
+++ assets/index.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event) => {
+    message1.textContent = ''
+    message2.textContent = ''
+    const weatherForm = document.querySelector('form');
+    event.preventDefault();
+    console.log(event)
+    let loc = search.value;
+    fetch('http://localhost:3000/weather?address='+loc).then((res) => {
+        res.json().then((data) => {
+            console.log(data)
+            if (data.error) {
+                console.log(data.error);
+                message2.textContent = data.error;
+            }
+            else {
+                console.log(data.response);
+                message1.textContent = data.location
+                message2.textContent = data.response;    
+            }
+        })
+    })
+})
+
---
+++ src/app.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geoCode = require('./utils/geocode')
+const foreCast = require('./utils/forecast')
+
+const app = express();
+
+const indexPath = path.join(__dirname, '../assets')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(indexPath));
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Andrew Mead'
+    });
+})
+app.get('/weather', (req, res) => {
+    let address = req.query.address
+    if (!address) {
+        return res.send({ error: 'No Address found' })
+    }
+    geoCode(address, (error, { latitude, longitude, location }={}) => {
+        if(error){
+            // return res.render('error',{err:'GeoCode Error'})
+            return res.send({error:'GeoCode Error'})
+        }   
+        foreCast(latitude,longitude,(error,response)=>{
+            if(error){
+                // return res.render('error',{err:'Forecasting Error'});
+                return res.send({error:'Forecasting Error'})
+            }
+            // return res.render('weatherReport',{response,location});
+            return res.send({response,location})
+        })
+    })
+    // res.send({ address: address })
+})
+app.get('/weather/*', (req, res) => {
+    res.render('gotoweather', { text: 'Go to Weather Page', link: '/weather' })
+})
+app.get('*', (req, res) => {
+    res.send('My 404')
+})
+app.listen(3000, () => {
+    console.log('Serer running on port 3k')
+})
